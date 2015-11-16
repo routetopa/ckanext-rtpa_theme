f916fda056c2d3cc1425d8697c38d4cfd5341e7e
Arkadiusz Stasiewicz <arkadiusz.stasiewicz@deri.org>
2015-11-16 11:57:54 +0000

    IRoutes configuration

--- ckanext/rtpa_theme/plugin.py
+++ ckanext/rtpa_theme/plugin.py
@@ -4,17 +4,16 @@ import ckan.plugins.toolkit as toolkit
 
 class Rtpa_ThemePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
+    plugins.implements(plugins.IRoutes, inherit=True)
 
     # IConfigurer
-
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
         toolkit.add_public_directory(config, 'public')
-
         toolkit.add_resource('fanstatic', 'rtpa_theme')
 
     ## IRoutes
     def before_map(self, map):
         controller = 'ckanext.rtpa_theme.controllers:ViewController'
-        map.redirect('/', '/dataset')
+        # map.redirect('/', '/dataset')
         return map
